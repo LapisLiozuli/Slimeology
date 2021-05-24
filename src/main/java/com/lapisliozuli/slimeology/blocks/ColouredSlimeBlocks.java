cae779f3d5649b258e77326f02a7f1f1876251b8
LapisLiozuli <70147943+LapisLiozuli@users.noreply.github.com>
2021-05-24 20:48:24 +0800

    When R-Clicked with a Bleaching Brush, Coloured Slime Blocks are converted to vanilla Slime Blocks while dropping one of their respective Dyes. Rainbow Slime Block drops an extra Slimeball.

--- src/main/java/com/lapisliozuli/slimeology/blocks/ColouredSlimeBlocks.java
+++ src/main/java/com/lapisliozuli/slimeology/blocks/ColouredSlimeBlocks.java
@@ -3,6 +3,7 @@ package com.lapisliozuli.slimeology.blocks;
 import com.lapisliozuli.slimeology.Slimeology;
 import com.lapisliozuli.slimeology.items.BleachingBrush;
 import com.lapisliozuli.slimeology.items.SlimyToolBase;
+import com.lapisliozuli.slimeology.registry.RegisterItems;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
 import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
@@ -21,6 +22,7 @@ import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.predicate.item.ItemPredicate;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.tag.FluidTags;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
@@ -29,36 +31,41 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
 
 import java.util.*;
 
 
 public class ColouredSlimeBlocks extends SlimeBlock {
-    public ColouredSlimeBlocks() {
+    private Item csbDyeItem;
+
+    public ColouredSlimeBlocks(Item csbDyeItem) {
             super(FabricBlockSettings.copyOf(Blocks.SLIME_BLOCK).nonOpaque());
+        this.csbDyeItem = csbDyeItem;
     }
 
-    public Item csbDyeItem = Items.BLACK_DYE;
+//    public Item csbDyeItem = Items.BLACK_DYE;
 
     // Mass declare variables of the same type
-    public static final ColouredSlimeBlocks SLIME_BLOCK_DEBUG = new ColouredSlimeBlocks(),
-                                    SLIME_BLOCK_WHITE = new ColouredSlimeBlocks(),
-                                    SLIME_BLOCK_ORANGE = new ColouredSlimeBlocks(),
-                                    SLIME_BLOCK_MAGENTA = new ColouredSlimeBlocks(),
-                                    SLIME_BLOCK_LIGHT_BLUE = new ColouredSlimeBlocks(),
-                                    SLIME_BLOCK_YELLOW = new ColouredSlimeBlocks(),
-                                    SLIME_BLOCK_LIME = new ColouredSlimeBlocks(),
-                                    SLIME_BLOCK_PINK = new ColouredSlimeBlocks(),
-                                    SLIME_BLOCK_GRAY = new ColouredSlimeBlocks(),
-                                    SLIME_BLOCK_LIGHT_GRAY = new ColouredSlimeBlocks(),
-                                    SLIME_BLOCK_CYAN = new ColouredSlimeBlocks(),
-                                    SLIME_BLOCK_PURPLE = new ColouredSlimeBlocks(),
-                                    SLIME_BLOCK_BLUE = new ColouredSlimeBlocks(),
-                                    SLIME_BLOCK_BROWN = new ColouredSlimeBlocks(),
-                                    SLIME_BLOCK_GREEN = new ColouredSlimeBlocks(),
-                                    SLIME_BLOCK_RED = new ColouredSlimeBlocks(),
-                                    SLIME_BLOCK_BLACK = new ColouredSlimeBlocks(),
-                                    SLIME_BLOCK_RAINBOW = new ColouredSlimeBlocks();
+    public static final ColouredSlimeBlocks SLIME_BLOCK_DEBUG = new ColouredSlimeBlocks(RegisterItems.SLIME_BALL_DEBUG),
+                                    SLIME_BLOCK_WHITE = new ColouredSlimeBlocks(Items.WHITE_DYE),
+                                    SLIME_BLOCK_ORANGE = new ColouredSlimeBlocks(Items.ORANGE_DYE),
+                                    SLIME_BLOCK_MAGENTA = new ColouredSlimeBlocks(Items.MAGENTA_DYE),
+                                    SLIME_BLOCK_LIGHT_BLUE = new ColouredSlimeBlocks(Items.LIGHT_BLUE_DYE),
+                                    SLIME_BLOCK_YELLOW = new ColouredSlimeBlocks(Items.YELLOW_DYE),
+                                    SLIME_BLOCK_LIME = new ColouredSlimeBlocks(Items.LIME_DYE),
+                                    SLIME_BLOCK_PINK = new ColouredSlimeBlocks(Items.PINK_DYE),
+                                    SLIME_BLOCK_GRAY = new ColouredSlimeBlocks(Items.GRAY_DYE),
+                                    SLIME_BLOCK_LIGHT_GRAY = new ColouredSlimeBlocks(Items.LIGHT_GRAY_DYE),
+                                    SLIME_BLOCK_CYAN = new ColouredSlimeBlocks(Items.CYAN_DYE),
+                                    SLIME_BLOCK_PURPLE = new ColouredSlimeBlocks(Items.PURPLE_DYE),
+                                    SLIME_BLOCK_BLUE = new ColouredSlimeBlocks(Items.BLUE_DYE),
+                                    SLIME_BLOCK_BROWN = new ColouredSlimeBlocks(Items.BROWN_DYE),
+                                    SLIME_BLOCK_GREEN = new ColouredSlimeBlocks(Items.GREEN_DYE),
+                                    SLIME_BLOCK_RED = new ColouredSlimeBlocks(Items.RED_DYE),
+                                    SLIME_BLOCK_BLACK = new ColouredSlimeBlocks(Items.BLACK_DYE),
+                                    // Maybe drop a random dye eventually?
+                                    SLIME_BLOCK_RAINBOW = new ColouredSlimeBlocks(Items.SLIME_BALL);
 
     // Puts all Slimeblocks into a map for bulk handling.
     public static Map<String, ColouredSlimeBlocks> imperative() {
@@ -88,13 +95,17 @@ public class ColouredSlimeBlocks extends SlimeBlock {
 
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         ItemStack itemStack = player.getStackInHand(hand);
+        // Checks for Bleaching Brush.
         if (itemStack.getItem() == BleachingBrush.BLEACHING_BRUSH) {
-            if (!world.isClient) {
+            // Checks for Water Block nearby.
+            if (!world.isClient && isWaterNearby(world, pos)) {
                 Direction direction = hit.getSide();
                 Direction direction2 = direction.getAxis() == Direction.Axis.Y ? player.getHorizontalFacing().getOpposite() : direction;
+                // Creates sound.
                 world.playSound((PlayerEntity)null, pos, SoundEvents.BLOCK_GRASS_PLACE, SoundCategory.BLOCKS, 1.0F, 1.0F);
                 world.setBlockState(pos, (BlockState)Blocks.SLIME_BLOCK.getDefaultState(), 11);
-                ItemEntity itemEntity = new ItemEntity(world, (double)pos.getX() + 0.5D + (double)direction2.getOffsetX() * 0.65D, (double)pos.getY() + 0.1D, (double)pos.getZ() + 0.5D + (double)direction2.getOffsetZ() * 0.65D, new ItemStack(csbDyeItem, 4));
+                // Drops dyeItem.
+                ItemEntity itemEntity = new ItemEntity(world, (double)pos.getX() + 0.5D + (double)direction2.getOffsetX() * 0.65D, (double)pos.getY() + 0.1D, (double)pos.getZ() + 0.5D + (double)direction2.getOffsetZ() * 0.65D, new ItemStack(csbDyeItem, 1));
                 itemEntity.setVelocity(0.05D * (double)direction2.getOffsetX() + world.random.nextDouble() * 0.02D, 0.05D, 0.05D * (double)direction2.getOffsetZ() + world.random.nextDouble() * 0.02D);
                 world.spawnEntity(itemEntity);
                 itemStack.damage(1, player, (playerEntity) -> {
@@ -108,10 +119,28 @@ public class ColouredSlimeBlocks extends SlimeBlock {
         }
     }
 
+
+    // Somehow need to refer to Moisture
+    private static boolean isWaterNearby(WorldView world, BlockPos pos) {
+        // Range is 1 block along horizontal axes.
+        Iterator var2 = BlockPos.iterate(pos.add(-1, 0, -1), pos.add(1, 0, 1)).iterator();
+
+        BlockPos blockPos;
+        do {
+            if (!var2.hasNext()) {
+                return false;
+            }
+
+            blockPos = (BlockPos)var2.next();
+        } while(!world.getFluidState(blockPos).isIn(FluidTags.WATER));
+
+        return true;
+    }
+
     public static List<BlockItem> csbBlockItemsList = new ArrayList<>(Collections.emptyList());;
 
-    // The block loot table can store conditions, but it's not strictly necessary to allow the block to drop itself in item form.
-    private static final Identifier SLIME_BLOCK_LOOT_TABLE_ID = new Identifier("minecraft", "blocks/slime_block");
+//    // The block loot table can store conditions, but it's not strictly necessary to allow the block to drop itself in item form.
+//    private static final Identifier SLIME_BLOCK_LOOT_TABLE_ID = new Identifier("minecraft", "blocks/slime_block");
 
     public static void registerSlimeBlocks() {
         colouredSlimeBlocksMap.forEach((k,v) ->
@@ -126,18 +155,19 @@ public class ColouredSlimeBlocks extends SlimeBlock {
 
         }
 
-        // Sets loot table of Slime Block when broken by Slimotic Duplicator to drop extra slimeballs.
-        LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, supplier, setter) -> {
-            if (SLIME_BLOCK_LOOT_TABLE_ID.equals(id)) {
-                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
-                        // That's all I need. Amazing. So this is adding to the current loot table, not overriding it.
-                        .rolls(UniformLootTableRange.between(0.0f, 4.0f))
-                        .withEntry(ItemEntry.builder(Items.SLIME_BALL).build())
-                        .conditionally(MatchToolLootCondition.builder(ItemPredicate.Builder.create().item(SlimyToolBase.SLIMOTIC_DUPLICATOR)));
-
-                supplier.withPool(poolBuilder.build());
-            }
-        });
+//        // Shouldn't this code be under the SlimyToolBase (Slimotic Duplicator)?
+//        // Sets loot table of Slime Block when broken by Slimotic Duplicator to drop extra slimeballs.
+//        LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, supplier, setter) -> {
+//            if (SLIME_BLOCK_LOOT_TABLE_ID.equals(id)) {
+//                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+//                        // That's all I need. Amazing. So this is adding to the current loot table, not overriding it.
+//                        .rolls(UniformLootTableRange.between(0.0f, 4.0f))
+//                        .withEntry(ItemEntry.builder(Items.SLIME_BALL).build())
+//                        .conditionally(MatchToolLootCondition.builder(ItemPredicate.Builder.create().item(SlimyToolBase.SLIMOTIC_DUPLICATOR)));
+//
+//                supplier.withPool(poolBuilder.build());
+//            }
+//        });
     }
 
     public static void renderSlimeBlocks() {
--- src/main/java/com/lapisliozuli/slimeology/items/BleachingBrush.java
+++ src/main/java/com/lapisliozuli/slimeology/items/BleachingBrush.java
@@ -31,62 +31,7 @@ public class BleachingBrush extends MiningToolItem {
     }
 
     private static final Set<Block> EFFECTIVE_BLOCKS;
-    protected static final Map<Block, BlockState> BLEACHABLE_BLOCKS;
-
-//    // Mimics how Hoes convert Dirt/Grass blocks to Farmland.
-//    public ActionResult useOnBlock(ItemUsageContext context) {
-//        World world = context.getWorld();
-//        BlockPos blockPos = context.getBlockPos();
-//        // Checks if the face is not the bottom face, and that there is Air above. (May remove second check because not thematic.)
-//        if (context.getSide() != Direction.DOWN && world.getBlockState(blockPos.up()).isAir()) {
-//            BlockState blockState = (BlockState) BLEACHABLE_BLOCKS.get(world.getBlockState(blockPos).getBlock());
-//            // Checks if a Water Block is nearby.
-//            if (blockState != null & isWaterNearby(world, blockPos)) {
-//                PlayerEntity playerEntity = context.getPlayer();
-//                // Creates sound of Grass block when Brush bleaches the CSB.
-//                world.playSound(playerEntity, blockPos, SoundEvents.BLOCK_GRASS_PLACE, SoundCategory.BLOCKS, 1.0F, 1.0F);
-//                if (!world.isClient) {
-//                    // Converts Bleachable Block to Slime Block.
-//                    world.setBlockState(blockPos, blockState, 11);
-//                    // Is there a way to drop a Dye Item at the blockPos above the CSB i.e. BlockPos.up()?
-//                    if (playerEntity != null) {
-//                        // Reduces tool durability by 1.
-//                        context.getStack().damage(1, playerEntity, (p) -> {
-//                            p.sendToolBreakStatus(context.getHand());
-//                        });
-//                    }
-//                }
-//                return ActionResult.success(world.isClient);
-//            }
-//        }
-//        return ActionResult.PASS;
-//    }
-
-
-    // Somehow need to refer to Moisture
-    private static boolean isWaterNearby(WorldView world, BlockPos pos) {
-        // Range is 1 block along horizontal axes.
-        Iterator var2 = BlockPos.iterate(pos.add(-1, 0, -1), pos.add(1, 0, 1)).iterator();
-
-        BlockPos blockPos;
-        do {
-            if (!var2.hasNext()) {
-                return false;
-            }
-
-            blockPos = (BlockPos)var2.next();
-        } while(!world.getFluidState(blockPos).isIn(FluidTags.WATER));
-
-        return true;
-    }
-
     static {
-        BLEACHABLE_BLOCKS = Maps.newHashMap();
-        // Adds all the Coloured Slime blocks to the list.
-        for (ColouredSlimeBlocks colouredSlimeBlock : ColouredSlimeBlocks.colouredSlimeBlocksMap.values())
-            // Links each Bleachable Block to the vanilla Slime Block.
-            BLEACHABLE_BLOCKS.put(colouredSlimeBlock, Blocks.SLIME_BLOCK.getDefaultState());
-
         // Just saying that the Brush isn't effective at breaking any blocks.
         EFFECTIVE_BLOCKS = ImmutableSet.of();
     }
