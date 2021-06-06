819f184f905882fe376416849a601fffa907c95b
LapisLiozuli <70147943+LapisLiozuli@users.noreply.github.com>
2021-06-06 18:28:09 +0800

    Updated PistonHandlerInjectMixin from Block parameter to Blockstate parameters.

--- src/main/java/com/lapisliozuli/slimeology/mixins/PistonHandlerInjectMixin.java
+++ src/main/java/com/lapisliozuli/slimeology/mixins/PistonHandlerInjectMixin.java
@@ -2,7 +2,7 @@ package com.lapisliozuli.slimeology.mixins;
 
 import com.lapisliozuli.slimeology.blocks.ColouredSlimeBlocks;
 import com.lapisliozuli.slimeology.registry.RegisterBlocks;
-import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.piston.PistonHandler;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,38 +14,38 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class PistonHandlerInjectMixin {
     // Gives the ColouredSlimeBlocks their sticky behaviour, and non-stickiness to other types of sticky blocks respectively.
     @Inject(at = @At("HEAD"), method = "isBlockSticky", cancellable = true)
-    private static void IsBlockStickyMixin(Block block, CallbackInfoReturnable callbackInfoReturnable) {
+    private static void IsBlockStickyMixin(BlockState state, CallbackInfoReturnable<Boolean> cir) {
         // Use RegisterItems to keep a list of all sticky blocks at some point.
         // Both CSBs and Slimy variants will pull blocks along.
-        if ((ColouredSlimeBlocks.colouredSlimeBlocksMap.containsValue(block))
-                || (RegisterBlocks.blocksList.contains(block))) {
-            callbackInfoReturnable.setReturnValue(true);
+        if ((ColouredSlimeBlocks.colouredSlimeBlocksMap.containsValue(state))
+                || (RegisterBlocks.blocksList.contains(state))) {
+            cir.setReturnValue(true);
         }
     }
 
-    // Kind of janky but it's following Mojang's logic0
+    // Kind of janky but it's following Mojang's logic.
     @Inject(at = @At("HEAD"), method = "isAdjacentBlockStuck", cancellable = true)
-    private static void IsAdjacentBlockStuckMixin(Block arg1, Block arg2, CallbackInfoReturnable callbackInfoReturnable) {
+    private static void IsAdjacentBlockStuckMixin(BlockState state, BlockState adjacentState, CallbackInfoReturnable<Boolean> cir) {
         // Rainbow Slime Block will pull any Slimeology sticky block along.
-        if ((arg1 == ColouredSlimeBlocks.SLIME_BLOCK_RAINBOW) | (arg2 == ColouredSlimeBlocks.SLIME_BLOCK_RAINBOW)) {
-            callbackInfoReturnable.setReturnValue(true);
+        if ((state.isOf(ColouredSlimeBlocks.SLIME_BLOCK_RAINBOW)) | (adjacentState.isOf(ColouredSlimeBlocks.SLIME_BLOCK_RAINBOW))) {
+            cir.setReturnValue(true);
         }
         // CSBs will not stick to Slime Blocks, Honey Blocks or Slimeology sticky blocks.
-        else if (((ColouredSlimeBlocks.colouredSlimeBlocksMap.containsValue(arg1)) &
-                (arg2 == Blocks.HONEY_BLOCK | arg2 == Blocks.SLIME_BLOCK | RegisterBlocks.blocksList.contains(arg2)))
-                | ((arg1 == Blocks.HONEY_BLOCK | arg1 == Blocks.SLIME_BLOCK | RegisterBlocks.blocksList.contains(arg1)) &
-                ColouredSlimeBlocks.colouredSlimeBlocksMap.containsValue(arg2))) {
-            callbackInfoReturnable.setReturnValue(false);
+        else if (((ColouredSlimeBlocks.colouredSlimeBlocksMap.containsValue(state)) &
+                (adjacentState.isOf(Blocks.HONEY_BLOCK) | adjacentState.isOf(Blocks.SLIME_BLOCK) | RegisterBlocks.blocksList.contains(adjacentState)))
+                | ((state.isOf(Blocks.HONEY_BLOCK) | state.isOf(Blocks.SLIME_BLOCK) | RegisterBlocks.blocksList.contains(state)) &
+                ColouredSlimeBlocks.colouredSlimeBlocksMap.containsValue(adjacentState))) {
+            cir.setReturnValue(false);
         }
         // Slimeology sticky blocks will not stick to Honey Blocks. Note: Can stick to Slime Blocks.
-        else if ((RegisterBlocks.blocksList.contains(arg1) && arg2 == Blocks.HONEY_BLOCK) |
-                (arg1 == Blocks.HONEY_BLOCK && RegisterBlocks.blocksList.contains(arg2))) {
-            callbackInfoReturnable.setReturnValue(false);
+        else if ((RegisterBlocks.blocksList.contains(state) && adjacentState.isOf(Blocks.HONEY_BLOCK)) |
+                (state.isOf(Blocks.HONEY_BLOCK) && RegisterBlocks.blocksList.contains(adjacentState))) {
+            cir.setReturnValue(false);
         }
         // CSBs of different colours will not stick to each other.
-        else if ((arg1 != arg2) & (ColouredSlimeBlocks.colouredSlimeBlocksMap.containsValue(arg1))
-                & (ColouredSlimeBlocks.colouredSlimeBlocksMap.containsValue(arg2))) {
-            callbackInfoReturnable.setReturnValue(false);
+        else if ((state != adjacentState) & (ColouredSlimeBlocks.colouredSlimeBlocksMap.containsValue(state))
+                & (ColouredSlimeBlocks.colouredSlimeBlocksMap.containsValue(adjacentState))) {
+            cir.setReturnValue(false);
         }
     }
 }
