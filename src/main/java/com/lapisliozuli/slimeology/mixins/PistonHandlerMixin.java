2a072b41843fa6dba5b0c28ca05dd68fb9c0a136
LapisLiozuli <70147943+LapisLiozuli@users.noreply.github.com>
2021-06-10 11:23:59 +0800

    Refactored boolean variables and added 'slimeology$' prefix to mixin methods to aid modpack debugging.

--- src/main/java/com/lapisliozuli/slimeology/mixins/PistonHandlerMixin.java
+++ src/main/java/com/lapisliozuli/slimeology/mixins/PistonHandlerMixin.java
@@ -12,13 +12,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PistonHandler.class)
 public class PistonHandlerMixin {
-    // Gives the ColouredSlimeBlocks their sticky behaviour.
+    // Gives sticky behaviour to the ColouredSlimeBlocks and Slimy variants.
     @Inject(at = @At("HEAD"), method = "isBlockSticky", cancellable = true)
-    private static void IsBlockStickyMixin(BlockState state, CallbackInfoReturnable<Boolean> cir) {
+    private static void slimeology$isBlockStickyMixin(BlockState state, CallbackInfoReturnable<Boolean> cir) {
         // Use RegisterItems to keep a list of all sticky blocks at some point.
         // Both CSBs and Slimy variants will pull blocks along.
-        if ((ColouredSlimeBlocks.colouredSlimeBlocksMap.containsValue(state.getBlock()))
-                || (RegisterBlocks.blocksList.contains(state.getBlock()))) {
+        if (ColouredSlimeBlocks.colouredSlimeBlocksMap.containsValue(state.getBlock())
+                || RegisterBlocks.blocksList.contains(state.getBlock())) {
             cir.setReturnValue(true);
         }
     }
@@ -26,27 +26,35 @@ public class PistonHandlerMixin {
     // Gives the ColouredSlimeBlocks non-stickiness to other types of sticky blocks.
     // Kind of janky but it's following Mojang's logic.
     @Inject(at = @At("HEAD"), method = "isAdjacentBlockStuck", cancellable = true)
-    private static void IsAdjacentBlockStuckMixin(BlockState state, BlockState adjacentState, CallbackInfoReturnable<Boolean> cir) {
-        // Rainbow Slime Block will pull any Slimeology sticky block along.
-        if ((state.getBlock() == ColouredSlimeBlocks.SLIME_BLOCK_RAINBOW) | adjacentState.getBlock() == ColouredSlimeBlocks.SLIME_BLOCK_RAINBOW) {
+    private static void slimeology$isAdjacentBlockStuckMixin(BlockState state, BlockState adjacentState, CallbackInfoReturnable<Boolean> cir) {
+        boolean isStateHoney = state.getBlock() == Blocks.HONEY_BLOCK;
+        boolean isAdjacentStateHoney = adjacentState.getBlock() == Blocks.HONEY_BLOCK;
+        boolean isStateCsb = ColouredSlimeBlocks.colouredSlimeBlocksMap.containsValue(state.getBlock());
+        boolean isAdjacentStateCsb = ColouredSlimeBlocks.colouredSlimeBlocksMap.containsValue(adjacentState.getBlock());
+        boolean isStateSlimyVariant = RegisterBlocks.blocksList.contains(state.getBlock());
+        boolean isAdjacentStateSlimyVariant = RegisterBlocks.blocksList.contains(adjacentState.getBlock());
+
+        // Rainbow Slime Block will pull any sticky block along.
+        if (state.getBlock() == ColouredSlimeBlocks.SLIME_BLOCK_RAINBOW
+                || adjacentState.getBlock() == ColouredSlimeBlocks.SLIME_BLOCK_RAINBOW) {
             cir.setReturnValue(true);
         }
         // CSBs will not stick to Slime Blocks, Honey Blocks or Slimeology sticky blocks.
-        else if (((ColouredSlimeBlocks.colouredSlimeBlocksMap.containsValue(state.getBlock()))
-                & (adjacentState.getBlock() == Blocks.HONEY_BLOCK | adjacentState.getBlock() == Blocks.SLIME_BLOCK | RegisterBlocks.blocksList.contains(adjacentState.getBlock())))
+        else if ((isStateCsb & (isAdjacentStateHoney || adjacentState.getBlock() == Blocks.SLIME_BLOCK || isAdjacentStateSlimyVariant))
                 // First condition and second condition are the same except state and adjacentState are swapped.
-                | ((state.getBlock() == Blocks.HONEY_BLOCK | state.getBlock() == Blocks.SLIME_BLOCK | RegisterBlocks.blocksList.contains(state.getBlock()))
-                & ColouredSlimeBlocks.colouredSlimeBlocksMap.containsValue(adjacentState.getBlock()))) {
+                || ((isStateHoney || state.getBlock() == Blocks.SLIME_BLOCK || isStateSlimyVariant) & isAdjacentStateCsb)) {
             cir.setReturnValue(false);
         }
         // Slimeology sticky blocks will not stick to Honey Blocks. Note: Can stick to Slime Blocks.
-        else if ((RegisterBlocks.blocksList.contains(state.getBlock()) && adjacentState.getBlock() == Blocks.HONEY_BLOCK) |
-                (state.getBlock() == Blocks.HONEY_BLOCK) && (RegisterBlocks.blocksList.contains(adjacentState.getBlock()))) {
+        // Check for the combo of a Sticky variant and a Honey Block.
+        else if ((isStateSlimyVariant && isAdjacentStateHoney)
+                || (isStateHoney && isAdjacentStateSlimyVariant)) {
             cir.setReturnValue(false);
         }
         // CSBs of different colours will not stick to each other.
-        else if ((state != adjacentState) & (ColouredSlimeBlocks.colouredSlimeBlocksMap.containsValue(state.getBlock()))
-                & (ColouredSlimeBlocks.colouredSlimeBlocksMap.containsValue(adjacentState.getBlock()))) {
+        // Both blocks must be different. (Technically this check applies to previous checks too, but better to avoid nested ifs.)
+        else if ((state != adjacentState)
+                && (isStateCsb && isAdjacentStateCsb)) {
             cir.setReturnValue(false);
         }
     }
