bd52410e901b52413a31480499b0412b93e1fd16
LapisLiozuli <70147943+LapisLiozuli@users.noreply.github.com>
2021-06-10 21:55:11 +0800

    PushEntitiesMixin rewritten from @Overwrite to @Inject, locals and @ModifyVariable.

--- src/main/java/com/lapisliozuli/slimeology/mixins/PushEntitiesMixin.java
+++ src/main/java/com/lapisliozuli/slimeology/mixins/PushEntitiesMixin.java
@@ -18,14 +18,11 @@ import java.util.List;
 @Mixin(PistonBlockEntity.class)
 public class PushEntitiesMixin {
     private static boolean isBlockSlimy;
-    private static Block pushedBlock;
+    private static Block accessedPushedBlock;
 
     @Inject(
             method = "pushEntities",
-//            at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z", shift = At.Shift.AFTER),
             at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z", shift = At.Shift.AFTER),
-//            at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/entity/Entity;getPistonBehavior()Lnet/minecraft/block/piston/PistonBehavior;", shift = At.Shift.AFTER),
-//            locals = LocalCapture.PRINT,
             locals = LocalCapture.CAPTURE_FAILSOFT,
             cancellable = true
     )
@@ -33,31 +30,17 @@ public class PushEntitiesMixin {
                                                  Direction direction, double d, VoxelShape voxelShape, Box box, List list, List list2,
                                                  boolean bl) {
         isBlockSlimy = bl;
-        pushedBlock = blockEntity.getPushedBlock().getBlock();
-//        System.out.println("changeBoolean");
-//        System.out.println(isBlockSlimy);
-//        System.out.println(pushedBlock);
+        accessedPushedBlock = blockEntity.getPushedBlock().getBlock();
     }
 
     @ModifyVariable(
             print = true,
             method = "pushEntities",
-//            slice = ,
             at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z", shift = At.Shift.AFTER),
-//            at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z"),
             index = 11,
             ordinal = 0
-//            name = "bl"
     )
     private static boolean slimeology$pushEntitiesMixin(boolean bl) {
-//    private static Boolean slimeology$pushEntitiesMixin(boolean bl) {
-//    private static Boolean slimeology$pushEntitiesMixin(World world, BlockPos pos, float f, PistonBlockEntity blockEntity, CallbackInfo ci,
-//                                                        Direction direction, double d, VoxelShape voxelShape, Box box, List list, List list2,
-//                                                        boolean bl) {
-        System.out.println("pushEntities");
-        System.out.println(isBlockSlimy);
-        System.out.println("+++++++");
-        System.out.println(pushedBlock);
-        return isBlockSlimy || ColouredSlimeBlocks.colouredSlimeBlocksMap.containsValue(pushedBlock);
+        return isBlockSlimy || ColouredSlimeBlocks.colouredSlimeBlocksMap.containsValue(accessedPushedBlock);
     }
 }
